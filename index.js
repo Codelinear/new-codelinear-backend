07365951ec13bcf18b5e1cbf13c44e343b3f45aa
codelinear <152366209+Codelinear@users.noreply.github.com>
2024-04-01 18:33:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,21 +15,46 @@ if (cluster.isMaster) {
 } else {
     process.env.UV_THREADPOOL_SIZE = 32;
     const express = require('express');
+    const session = require('express-session');
     const cors = require('cors');
     const { router } = require('./routes/routes');
     const bodyParser = require('body-parser');
     const path = require('path');
 
     const app = express();
+
+    const sessionMiddleware = session({
+        secret: 'rewvtgreytbryjjgbn6yr',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false } 
+    });
+
+    app.use(sessionMiddleware);
+
     app.use(express.json());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(cors());
     app.use(router);
     app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
     const port = process.env.PORT || 4500;
-    app.listen(port, () => {
+    const server = app.listen(port, () => {
         console.log(`Worker ${process.pid} is running`);
-        console.log(`app running on port ${port}...`);
+        console.log(`App running on port ${port}...`);
+    });
+
+    process.on('exit', () => {
+        server.close(() => {
+            console.log(`Worker ${process.pid} closed`);
+        });
+    });
+
+    cluster.on('disconnect', (worker) => {
+        console.log(`Worker ${worker.process.pid} disconnected`);
+        sessionMiddleware.destroy(() => {
+            console.log(`Session destroyed for Worker ${worker.process.pid}`);
+        });
     });
 }
