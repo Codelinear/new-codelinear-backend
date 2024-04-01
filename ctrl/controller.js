1d9f8af35c2a5928e017c46308a49c4182b4d4c2
codelinear <152366209+Codelinear@users.noreply.github.com>
2024-04-01 10:58:17 +0530

    Update controller.js

--- ctrl/controller.js
+++ ctrl/controller.js
@@ -3,8 +3,6 @@ var validator = require("node-email-validation");
 const mysql = require("mysql");
 const { response } = require("express");
 const multer = require("multer");
-// const NodeCache = require("node-cache");
-// const cache = new NodeCache({ stdTTL: 0 });
 
 const pool = mysql.createPool({
   host: "217.21.87.205",
@@ -13,89 +11,87 @@ const pool = mysql.createPool({
   database: "u947451844_pages",
 });
 
-const executeQuery = (sql, params, callback) => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error("Error connecting to database:", err);
-      callback(err, null);
-    } else {
-      connection.query(sql, params, (err, results) => {
-        connection.release();
-        if (err) {
-          console.error("Error executing query:", err);
-          callback(err, null);
-        } else {
-          callback(null, results);
-        }
-      });
-    }
+const executeQuery = (sql, params) => {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        console.error("Error connecting to database:", err);
+        reject(err);
+      } else {
+        connection.query(sql, params, (err, results) => {
+          connection.release();
+          if (err) {
+            console.error("Error executing query:", err);
+            reject(err);
+          } else {
+            resolve(results);
+          }
+        });
+      }
+    });
   });
 };
 
-const fetchDataByTableName = (tableName) => (req, res) => {
-  // const cacheKey = `${tableName}_data`;
-  // const cachedData = cache.get(cacheKey);
-  // if (cachedData) {
-  //   console.log("Data retrieved from cache");
-  //   return res.send(cachedData);
-  // }
-  const sql = `SELECT * FROM ${tableName}`;
-  executeQuery(sql, null, (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("Error retrieving data from database");
-      return;
-    }
-    // cache.set(cacheKey, results);
+const fetchDataByTableName = (tableName) => async (req, res) => {
+  try {
+    const sql = `SELECT * FROM ${tableName}`;
+    const results = await executeQuery(sql, null);
     res.send(results);
-  });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error retrieving data from database");
+  }
 };
 
-const insertDataIntoTable = (tableName) => (req, res) => {
+const insertDataIntoTable = (tableName) => async (req, res) => {
   const formData = req.body;
   const sql = `INSERT INTO ${tableName} SET ?`;
-  executeQuery(sql, formData, (err, result) => {
-    if (err) {
-      console.error("Error inserting data:", err);
-      res.status(500).send("Error inserting data into database");
-    } else {
-      console.log("Form data inserted:", result);
-      res.send("Form submitted successfully");
-    }
-  });
+
+  try {
+    const result = await executeQuery(sql, formData);
+    console.log("Form data inserted:", result);
+    res.send("Form submitted successfully");
+  } catch (err) {
+    console.error("Error inserting data:", err);
+    res.status(500).send("Error inserting data into database");
+  }
 };
-const deleteDataByColumnName = (tableName, columnName) => (req, res) => {
+
+const deleteDataByColumnName = (tableName, columnName) => async (req, res) => {
   const { value } = req.body;
 
   const sql = `DELETE FROM ${tableName} WHERE ${columnName} = ?`;
-  executeQuery(sql, [value], (err, result) => {
-    if (err) {
-      console.error(`Error deleting data from ${tableName}:`, err);
-      res.status(500).send(`Error deleting data from ${tableName}`);
-    } else {
-      console.log("Data deleted successfully:", result);
-      res.send("Data deleted successfully");
-    }
-  });
+
+  try {
+    const result = await executeQuery(sql, [value]);
+    console.log("Data deleted successfully:", result);
+    res.send("Data deleted successfully");
+  } catch (err) {
+    console.error(`Error deleting data from ${tableName}:`, err);
+    res.status(500).send(`Error deleting data from ${tableName}`);
+  }
 };
-const updateCaseStudyData = (id) => (req, res) => {
+const updateCaseStudyData = (id) => async (req, res) => {
   const { company, companytitle, companybody, companyindustry } = req.body;
 
   const sql = `UPDATE maincasestudy SET company = ?, companytitle = ?, companybody = ?, companyindustry = ? WHERE id = ?`;
-  executeQuery(
-    sql,
-    [company, companytitle, companybody, companyindustry, id],
-    (err, result) => {
-      if (err) {
-        console.error("Error updating case study data:", err);
-        res.status(500).send("Error updating case study data");
-      } else {
-        console.log("Case study data updated successfully:", result);
-        res.send("Case study data updated successfully");
-      }
-    }
-  );
+
+  try {
+    const result = await executeQuery(sql, [
+      company,
+      companytitle,
+      companybody,
+      companyindustry,
+      id,
+    ]);
+    console.log("Case study data updated successfully:", result);
+    res.send("Case study data updated successfully");
+  } catch (err) {
+    console.error("Error updating case study data:", err);
+    res.status(500).send("Error updating case study data");
+  }
 };
+
 const sendMailContact = (req, res) => {
   const { username, lastname, email, message } = req.body;
   if (validator.is_email_valid(email)) {
@@ -127,18 +123,19 @@ const sendMailContact = (req, res) => {
 };
 const sendMail = async (req, res) => {
   const { email, username } = req.body;
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "marketing@codelinear.com",
-      pass: "mdjj rmcm gqtc kuyt",
-    },
-  });
-  const mailOptions = {
-    from: "Codelinear <marketing@codelinear.com>",
-    to: email,
-    subject: " Thank You for Contacting Codelinear!",
-    text: `Dear ${username},
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "marketing@codelinear.com",
+        pass: "mdjj rmcm gqtc kuyt",
+      },
+    });
+    const mailOptions = {
+      from: "Codelinear <marketing@codelinear.com>",
+      to: email,
+      subject: " Thank You for Contacting Codelinear!",
+      text: `Dear ${username},
 
 Thank you for reaching out to Codelinear! 
 We appreciate you taking the time to connect with us.
@@ -151,22 +148,24 @@ Thank you again for choosing Codelinear. We look forward to assisting you furthe
 Best regards,
 
 Codelinear`,
-  };
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send("Error sending email");
-    } else {
-      console.log("Email sent:", info.response);
-      res.status(200).send("Email sent successfully");
-    }
-  });
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info.response);
+    res.status(200).send("Email sent successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error sending email");
+  }
 };
 const getMailData = insertDataIntoTable("contactforminformation");
 const getAdminData = insertDataIntoTable("manageadmin");
 const displayAdminData = fetchDataByTableName("manageadmin");
 const deleteAdminData = deleteDataByColumnName("manageadmin", "email");
-const deletePositionData = deleteDataByColumnName("careerpos1", "positiontitle1");
+const deletePositionData = deleteDataByColumnName(
+  "careerpos1",
+  "positiontitle1"
+);
 const displayMailData = fetchDataByTableName("contactforminformation");
 const getHomeHeroHeaderData = insertDataIntoTable("home_page");
 const displayHomeHeroHeaderData = fetchDataByTableName("home_page");
