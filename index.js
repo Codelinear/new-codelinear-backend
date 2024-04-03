b64a8e681b4c97c2e9a9a8c93db505ae7bf95b07
codelinear <152366209+Codelinear@users.noreply.github.com>
2024-04-03 18:47:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 const { router } = require("./routes/routes");
 const bodyParser = require("body-parser");
 const path = require("path");
-const mysql = require("mysql");
 const app = express();
 
 const sessionMiddleware = session({
@@ -43,24 +42,3 @@ process.on("uncaughtException", (err) => {
   console.error("Uncaught Exception:", err.message);
   process.exit(1);
 });
-
-const pool = mysql.createPool({
-  host: "217.21.87.205",
-  user: "u947451844_saif08",
-  password: "u]1ro&X$1R",
-  database: "u947451844_pages",
-  connectionLimit: 20,
-});
-
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error("Database connection error:", err.message);
-    process.exit(1);
-  }
-  console.log("Database Connected");
-  connection.release();
-});
-
-module.exports = {
-  pool,
-};
