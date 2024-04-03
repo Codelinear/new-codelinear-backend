b280b201ff9098281e58c99c30ed42ab5784e508
codelinear <152366209+Codelinear@users.noreply.github.com>
2024-04-03 18:53:53 +0530

    Update pool.js

--- pool/pool.js
+++ pool/pool.js
@@ -8,4 +8,13 @@ const pool = mysql.createPool({
   connectionLimit: 20,
 });
 
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Database connection error:", err.message);
+    process.exit(1);
+  }
+  console.log("Database Connected");
+  connection.release();
+});
+
 module.exports = pool;
