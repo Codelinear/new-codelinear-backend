9cf9a2c80cb865196734125ba484820822897daa
codelinear <152366209+Codelinear@users.noreply.github.com>
2024-04-05 12:03:27 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,7 @@
 const express = require("express");
-const multer = require("multer");
-const path = require("path");
-const mysql = require("mysql");
-const pool = require("../pool/pool");
-const NodeCache = require("node-cache");
-const cache = new NodeCache({ stdTTL: 86400, checkperiod: 60 });
-
 const {
+  upload,
+  uploadImage,
   sendMailContact,
   sendMail,
   fetchDataByTableName,
@@ -14,38 +9,11 @@ const {
   deleteDataByColumnName,
   insertDataIntoTable,
 } = require("../ctrl/controller");
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
-const upload = multer({ storage });
+
 const router = express.Router();
-router.post("/images", upload.single("image"), (req, res) => {
-  if (!req.file) {
-    res.status(400).send("No file uploaded");
-    return;
-  }
-  const { filename } = req.file;
-  const { id } = req.body;
-  const sql = "UPDATE images SET filename = ? WHERE id = ?";
-  pool.query(sql, [filename, id], (err, result) => {
-    if (err) {
-      console.error("Error uploading image: ", err);
-      res.status(500).send("Error uploading image");
-      return;
-    }
-    cache.del("images");
-    console.log("Image uploaded successfully");
-    res.status(200).send("Image uploaded successfully");
-  });
-});
+
+router.post("/images", upload.single("image"), uploadImage);
+
 router.post("/sendMailContact", sendMailContact);
 router.post("/sendMail", sendMail);
 router.post("/insert/:tableName", (req, res) => {
@@ -64,4 +32,5 @@ router.get("/fetch/:tableName", (req, res) => {
   const { tableName } = req.params;
   fetchDataByTableName(tableName)(req, res);
 });
+
 module.exports = { router };
